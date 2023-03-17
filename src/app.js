6a1a41f52229123ea8754f1f3037d82649158d43
arat-arjat <anderaniel@gmail.com>
2023-03-17 15:57:18 +0000

    culminacion ejercicio

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let donde = when[Math.floor(Math.random() * when.length)];
+
+  //let suma2 = quien + " " + accion + " " + que + " " + donde;
+
+  let suma = `${quien} ${accion} ${que}  ${donde} `;
+
+  let elemento = document.getElementById("excuse");
+  elemento.innerText = suma;
 };
